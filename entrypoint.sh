bffaf610dea2528adc908d7792f3c8a21f2393b3
Faizan Parabtani <parabtanifaizan@gmail.com>
2026-01-26 15:28:09 -0500

    Changing port from 8000 to 8080

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ uv run python manage.py migrate --noinput
 
 # Start server
 echo "Starting Gunicorn..."
-exec uv run gunicorn verosnacks.wsgi:application --bind "0.0.0.0:${PORT:-8000}"
+exec uv run gunicorn verosnacks.wsgi:application --bind "0.0.0.0:${PORT:-8080}"
