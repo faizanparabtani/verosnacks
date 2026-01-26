a2bd2940c2fa083d290a69e058c28418d142ace5
Faizan Parabtani <parabtanifaizan@gmail.com>
2026-01-26 18:26:14 -0500

    Some Changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ set -e
 uv run python manage.py migrate --noinput
 
 # Start server
-uv run gunicorn -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${PORT:-8000}" verosnacks.wsgi:application
+exec uv run gunicorn -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:${PORT:-8000}" verosnacks.asgi:application
