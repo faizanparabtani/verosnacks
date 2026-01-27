2b766630f81c3bcbc2aa7294438cd98428666cab
Faizan Parabtani <parabtanifaizan@gmail.com>
2026-01-26 19:16:14 -0500

    Modified build files

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Run migrations
 uv run python manage.py migrate --noinput
 
+# Collect static files
+uv run python manage.py collectstatic --noinput
+
 # Start server
 exec uv run gunicorn -k uvicorn.workers.UvicornWorker --bind "0.0.0.0:8000" verosnacks.asgi:application
