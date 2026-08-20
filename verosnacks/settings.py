2ce4236374f9fe2631697005426f039382adf091
Faizan <parabtanifaizan@gmail.com>
2026-08-19 22:57:51 -0400

    Fixed Storages in settings

--- verosnacks/settings.py
+++ verosnacks/settings.py
@@ -108,6 +108,15 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "assets"]
 
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
+
 
 # Standard Settings
 LANGUAGE_CODE = "en-us"
