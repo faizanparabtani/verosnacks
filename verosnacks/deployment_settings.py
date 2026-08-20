9777363ca6675ede1ad2f86150e8806fa9e7048a
Faizan <parabtanifaizan@gmail.com>
2026-08-19 23:51:40 -0400

    test

--- verosnacks/deployment_settings.py
+++ verosnacks/deployment_settings.py
@@ -50,11 +50,11 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 cloudinary.config(
     cloud_name=os.environ.get("CLOUD_NAME"),
